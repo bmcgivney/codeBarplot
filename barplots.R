dfc90c2fd03336ec33b4543e74bd58bb472eaf5e
beatrice mcgivney <beatrice mcgivney>
2017-03-21 22:02:55 +0000

    Add percentages

--- barplots.R
+++ barplots.R
@@ -1,8 +1,20 @@
-read.table(file="DUN_SMG.txt", header = TRUE, sep="\t", stringsAsFactors = FALSE)->Test
-Test[which(Test$SGT == "CC"),]->CC_Test
-CC_Test$Genomic_Racing_Value
-mean(CC_Test$Genomic_Racing_Value, na.rm = TRUE)
+Test<-read.table(file="data/WHS.txt", header = TRUE, sep="\t", stringsAsFactors = FALSE)
 
-table(CC_Test$Genomic_Racing_Value)->GRV
-barplot(GRV)
+SGT<-table(Test$SGT)
+#n SGT
+SGT
+barplot(SGT)
+#%SGT
+round((prop.table(SGT)*100), 0)
 
+Test$Raced<-Test$TotalStarts
+Test$Raced[which(Test$Raced>0)]<-"Raced"
+Test$Raced[which(Test$Raced == 0)]<-"Unraced"
+Raced<-table(Test$Raced, Test$SGT)
+#n SGT
+Raced
+barplot(Raced)
+#%SGT
+#Use 1 to get row percentages and 2 to get column percentages
+RacedP<-round((prop.table(Raced, 2)*100), 0)
+barplot(RacedP)
